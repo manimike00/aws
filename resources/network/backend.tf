e2fb944f900eb5d0bd012567abf0edbfb923e0d9
Manikandan S <manikandan.subramanian@rapyder.com>
2024-04-17 13:30:02 +0530

    add vpc flowlogs to S3

--- resources/network/backend.tf
+++ resources/network/backend.tf
@@ -1,8 +1,8 @@
-terraform {
-  backend "s3" {
-    bucket         = ""
-    key            = ""
-    region         = ""
-    encrypt        = true
-  }
-}
+#terraform {
+#  backend "s3" {
+#    bucket         = ""
+#    key            = ""
+#    region         = ""
+#    encrypt        = true
+#  }
+#}
